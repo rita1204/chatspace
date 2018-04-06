b07bb07eb3dd77ff16fc4b4e259a5523da3d78dd
rita1204 <pittixichan@gmail.com>
2018-04-06 17:50:33 +0900

    add action:edit&update/change root_path

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit,:update]
   def new
     @group = Group.new
     @group.users << current_user
@@ -17,10 +18,19 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to root_path,notice: "グループを編集しました"
+    else
+      render'edit'
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
