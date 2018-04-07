a192a105fb198e4572642a0dd45593d3ab8f18fe
rita1204 <pittixichan@gmail.com>
2018-04-07 11:49:02 +0900

    create model/add association&validation/

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,5 @@ class Group < ApplicationRecord
   validates :name,presence:true
   has_many :group_users
   has_many :users, through: :group_users
-  # accepts_nested_attributes_for :group_users
+  has_many :messages
 end
